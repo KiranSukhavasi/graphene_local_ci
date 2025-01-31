f7c2a4d1715ffc5e8afab2b065e7141058129bf7
KiranSukhavasi <kiran.sukhavasi@intel.com>
2025-02-01 02:25:08 +0530

    Update helper.sh

--- gsc/test/ovms/helper.sh
+++ gsc/test/ovms/helper.sh
@@ -1,8 +1,3 @@
-docker build --tag ovms-bash --file  Examples/ovms/ovms-gsc.dockerfile .
-./gsc build --insecure-args ovms-bash  Examples/ovms/ovms.manifest
-./gsc sign-image ovms-bash enclave-key.pem
-./gsc info-image gsc-ovms-bash | tee gsc_ovms_image_info
-docker run gsc-ovms-bash -u $(id -u):$(id -g) -p 9000:9000 -v /mnt/tmpfs/model_encrypted:/mnt/tmpfs/model_encrypted
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/python/client_utils.py -o client_utils.py
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/face_detection/python/face_detection.py -o face_detection.py
 curl --create-dirs https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/static/images/people/people1.jpeg -o images/people1.jpeg
