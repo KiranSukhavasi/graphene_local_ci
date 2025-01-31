020ac3ff95d66a0792451e5d3f4a6bb8babf31b7
KiranSukhavasi <kiran.sukhavasi@intel.com>
2025-02-01 00:18:32 +0530

    Update helper.sh

--- gsc/test/ovms/helper.sh
+++ gsc/test/ovms/helper.sh
@@ -1,5 +1,5 @@
-docker build --tag ovms-bash --file ovms-gsc.dockerfile .
-./gsc build --insecure-args ovms-bash ovms.manifest
+docker build --tag ovms-bash --file  Examples/ovms/ovms-gsc.dockerfile .
+./gsc build --insecure-args ovms-bash  Examples/ovms/ovms.manifest
 ./gsc sign-image ovms-bash enclave-key.pem
 ./gsc info-image gsc-ovms-bash | tee gsc_ovms_image_info
 docker run gsc-ovms-bash -u $(id -u):$(id -g) -p 9000:9000 -v /mnt/tmpfs/model_encrypted:/mnt/tmpfs/model_encrypted
@@ -9,7 +9,7 @@ curl --create-dirs https://raw.githubusercontent.com/openvinotoolkit/model_serve
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
-pip install -r client_requirements.txt
+pip install -r  Examples/ovms/client_requirements.txt
 pip install "numpy<2.0"
 pip3 install opencv-python
-python3 face_detection.py --batch_size 1 --width 600 --height 400 --input_images_dir images --output_dir results --grpc_port 9000
+python3  Examples/ovms/face_detection.py --batch_size 1 --width 600 --height 400 --input_images_dir  Examples/ovms/images --output_dir  Examples/ovms/results --grpc_port 9000
