db412b0b09f33036f4b44b32fbc69ab8b9ccff8f
KiranSukhavasi <kiran.sukhavasi@intel.com>
2025-02-12 20:23:01 +0530

    Update commands.sh

--- gsc/test/ovms/commands.sh
+++ gsc/test/ovms/commands.sh
@@ -1,6 +1,7 @@
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/python/client_utils.py -o Examples/ovms/client_utils.py
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/face_detection/python/face_detection.py -o Examples/ovms/face_detection.py
 curl --create-dirs https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/static/images/people/people1.jpeg -o Examples/ovms/images/people1.jpeg
+mkdir Examples/ovms/results
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
@@ -9,12 +10,9 @@ export no_proxy=intel.com,.intel.com,localhost,127.0.0.1
 python3 Examples/ovms/face_detection.py --batch_size 1 --width 600 --height 400 --input_images_dir  Examples/ovms/images --output_dir  Examples/ovms/results --grpc_port 9000
 deactivate
 file_to_check="Examples/ovms/results/1_0.jpg"
-output_file="Examples/ovms/result.txt"
+output_file="Examples/ovms/ovms_result.txt"
 if [ -f "$file_to_check" ]; then
     echo "SUCCESS : File exists." > "$output_file"
 else
     echo "ERROR : File does not exist." > "$output_file"
 fi
-
-
-
